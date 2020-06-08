0e726371e2ef7a7970b34c5451a1817b0c9bf990
Tatsuro Shibamura <me@shibayan.jp>
2020-06-08 20:33:45 +0900

    Adding quote

--- AppService.Acmebot/SharedFunctions.cs
+++ AppService.Acmebot/SharedFunctions.cs
@@ -264,7 +264,7 @@ namespace AppService.Acmebot
                 // ファイルに今回のチャレンジが含まれていない場合もエラー
                 if (fileContent != challengeResult.HttpResourceValue)
                 {
-                    throw new RetriableActivityException($"{challengeResult.HttpResourceUrl} is not correct. Expected: {challengeResult.HttpResourceValue}, Actual: {fileContent}");
+                    throw new RetriableActivityException($"{challengeResult.HttpResourceUrl} is not correct. Expected: \"{challengeResult.HttpResourceValue}\", Actual: \"{fileContent}\"");
                 }
             }
         }
