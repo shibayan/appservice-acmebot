3e9c95731a057ce665456bbfe3db4820ef28916e
Tatsuro Shibamura <me@shibayan.jp>
2020-06-19 00:31:33 +0900

    Adding Acmebot options validation

--- AppService.Acmebot/AcmebotOptions.cs
+++ AppService.Acmebot/AcmebotOptions.cs
@@ -1,13 +1,19 @@
-﻿namespace AppService.Acmebot
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppService.Acmebot
 {
     public class AcmebotOptions
     {
+        [Required]
         public string Endpoint { get; set; } = "https://acme-v02.api.letsencrypt.org/";
 
+        [Required]
         public string Contacts { get; set; }
 
+        [Required]
         public string SubscriptionId { get; set; }
 
+        [Url]
         public string Webhook { get; set; }
     }
 }
