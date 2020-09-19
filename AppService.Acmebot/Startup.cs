9147a5664d4886fe0bce16febb04f97be7125da9
Tatsuro Shibamura <me@shibayan.jp>
2020-09-19 15:22:29 +0900

    Adding PreferredChain options

--- AppService.Acmebot/Startup.cs
+++ AppService.Acmebot/Startup.cs
@@ -91,7 +91,15 @@ namespace AppService.Acmebot
 
             builder.Services.AddOptions<AcmebotOptions>()
                    .Bind(section.Exists() ? section : context.Configuration.GetSection("LetsEncrypt"))
-                   .ValidateDataAnnotations();
+                   .ValidateDataAnnotations()
+                   .PostConfigure(options =>
+                   {
+                       // Backward compatibility
+                       if (options.Endpoint == "https://acme-v02.api.letsencrypt.org/")
+                       {
+                           options.PreferredChain ??= "DST Root CA X3";
+                       }
+                   });
         }
     }
 }
