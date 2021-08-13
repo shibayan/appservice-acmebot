3503f23fb7dc6aed83a6e032fe5c9c3a70aff3c1
Tatsuro Shibamura <me@shibayan.jp>
2021-08-13 16:26:54 +0900

    Fixed teams webhook issue and improvement format

--- AppService.Acmebot/Internal/WebhookInvoker.cs
+++ AppService.Acmebot/Internal/WebhookInvoker.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 using AppService.Acmebot.Options;
 
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+
+using Newtonsoft.Json;
 
 namespace AppService.Acmebot.Internal
 {
@@ -76,8 +79,8 @@ namespace AppService.Acmebot.Internal
             {
                 model = new
                 {
-                    title = $"{appName} - {slotName}",
-                    text = string.Join("\n", dnsNames),
+                    title = "Acmebot",
+                    text = $"A new certificate has been issued.\n\n**App Name**: {appName}\n\n**Slot Name**: {slotName}\n\n**Expiration Date**: {expirationDate}\n\n**DNS Names**: {string.Join(", ", dnsNames)}",
                     themeColor = "2EB886"
                 };
             }
@@ -123,8 +126,8 @@ namespace AppService.Acmebot.Internal
             {
                 model = new
                 {
-                    title = functionName,
-                    text = reason,
+                    title = "Acmebot",
+                    text = $"**{functionName}**\n\n**Reason**\n\n{reason}",
                     themeColor = "A30200"
                 };
             }
@@ -144,7 +147,9 @@ namespace AppService.Acmebot.Internal
         {
             var httpClient = _httpClientFactory.CreateClient();
 
-            var response = await httpClient.PostAsJsonAsync(_options.Webhook, model);
+            var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+
+            var response = await httpClient.PostAsync(_options.Webhook, content);
 
             if (!response.IsSuccessStatusCode)
             {
