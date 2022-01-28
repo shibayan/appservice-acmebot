9810a488619a9fad38ce933e8d31dc1fe42cbc74
Tatsuro Shibamura <me@shibayan.jp>
2022-01-28 16:06:38 +0900

    Adding `RenewBeforeExpiry` option

--- AppService.Acmebot/Options/AcmebotOptions.cs
+++ AppService.Acmebot/Options/AcmebotOptions.cs
@@ -5,6 +5,7 @@ namespace AppService.Acmebot.Options
     public class AcmebotOptions
     {
         [Required]
+        [Url]
         public string Endpoint { get; set; } = "https://acme-v02.api.letsencrypt.org/";
 
         [Required]
@@ -20,6 +21,9 @@ namespace AppService.Acmebot.Options
         public string Environment { get; set; } = "AzureCloud";
 
         public string PreferredChain { get; set; }
+
+        [Range(0, 365)]
+        public int RenewBeforeExpiry { get; set; } = 30;
 
         public ExternalAccountBindingOptions ExternalAccountBinding { get; set; }
     }
