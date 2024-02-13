b90f2136dbd0b207990da86244f3b1f74ca16680
Tatsuro Shibamura <me@shibayan.jp>
2024-02-14 04:04:40 +0900

    Complete prefer use Bearer Token

--- AppService.Acmebot/Internal/KuduClient.cs
+++ AppService.Acmebot/Internal/KuduClient.cs
@@ -1,35 +1,23 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
-
-using Azure.Core;
 
 namespace AppService.Acmebot.Internal;
 
 public class KuduClient
 {
-    public KuduClient(HttpClient httpClient, Uri scmUri, TokenCredential tokenCredential)
+    public KuduClient(HttpClient httpClient)
     {
         _httpClient = httpClient;
-        _scmHost = scmUri.Host;
-        _tokenCredential = tokenCredential;
     }
 
     private readonly HttpClient _httpClient;
-    private readonly string _scmHost;
-    private readonly TokenCredential _tokenCredential;
 
     public async Task<bool> ExistsFileAsync(string filePath)
     {
-        var accessToken = await _tokenCredential.GetTokenAsync(new TokenRequestContext(), CancellationToken.None);
-
-        var request = new HttpRequestMessage(HttpMethod.Head, $"https://{_scmHost}/api/vfs/site/{filePath}");
-
-        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.Token);
+        var request = new HttpRequestMessage(HttpMethod.Head, $"/api/vfs/site/{filePath}");
 
         var response = await _httpClient.SendAsync(request);
 
@@ -50,11 +38,8 @@ public class KuduClient
 
     public async Task WriteFileAsync(string filePath, string content)
     {
-        var accessToken = await _tokenCredential.GetTokenAsync(new TokenRequestContext(), CancellationToken.None);
+        var request = new HttpRequestMessage(HttpMethod.Put, $"/api/vfs/site/{filePath}");
 
-        var request = new HttpRequestMessage(HttpMethod.Put, $"https://{_scmHost}/api/vfs/site/{filePath}");
-
-        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.Token);
         request.Headers.IfMatch.Add(EntityTagHeaderValue.Any);
 
         request.Content = new StringContent(content, Encoding.UTF8);
--- AppService.Acmebot/Internal/KuduClientFactory.cs
+++ AppService.Acmebot/Internal/KuduClientFactory.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
 
 using Azure.Core;
 
@@ -7,19 +10,27 @@ namespace AppService.Acmebot.Internal;
 
 public class KuduClientFactory
 {
-    public KuduClientFactory(IHttpClientFactory httpClientFactory, TokenCredential tokenCredential)
+    public KuduClientFactory(IHttpClientFactory httpClientFactory, TokenCredential tokenCredential, AzureEnvironment environment)
     {
         _httpClientFactory = httpClientFactory;
         _tokenCredential = tokenCredential;
+        _environment = environment;
     }
 
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly TokenCredential _tokenCredential;
+    private readonly AzureEnvironment _environment;
 
-    public KuduClient CreateClient(Uri scmUri)
+    public async Task<KuduClient> CreateClientAsync(string scmHost)
     {
         var httpClient = _httpClientFactory.CreateClient();
 
-        return new KuduClient(httpClient, scmUri, _tokenCredential);
+        var context = new TokenRequestContext(new[] { _environment.ResourceManager.DefaultScope }, null);
+        var accessToken = await _tokenCredential.GetTokenAsync(context, CancellationToken.None);
+
+        httpClient.BaseAddress = new Uri($"https://{scmHost}");
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.Token);
+
+        return new KuduClient(httpClient);
     }
 }
